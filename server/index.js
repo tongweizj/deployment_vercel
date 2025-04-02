f09cfbbff2538bc10771200934a74bbd6c462596
tongwei <tongweizj@gmail.com>
2025-04-02 15:51:35 -0400

    feature: init project

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use("/",(req,res)=>{
+    res.send("server is running.");
+})
+app.listen(5000, console.log("Server started on PORT 5000"));
